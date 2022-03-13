b96adaff0614023d94b7fff5b74634c37c247c89
Samuel Mok <samopsa@gmail.com>
2022-03-13 16:25:40 +0100

    lotsa updates

--- timekeeping.py
+++ timekeeping.py
@@ -1,4 +1,5 @@
 from operator import truediv
+from socket import timeout
 import tkinter as tk
 import datetime
 import keyboard
@@ -7,15 +8,20 @@ import pandas as pd
 from hanging_threads import start_monitoring
 from typing import Optional
 from ctypes import wintypes, windll, create_unicode_buffer
-import openpyxl
+import win32gui, win32api, win32process, psutil, threading
 
-start_monitoring(seconds_frozen=10, test_interval=100)
+#enable for debugging hanging thread
+#start_monitoring(seconds_frozen=10, test_interval=100)
 
 """
-function to get the focused window title for autologging
-https://stackoverflow.com/questions/10266281/obtain-active-window-using-python
+GENERAL FUNCTIONS
 """
+
 def getForegroundWindowTitle() -> Optional[str]:
+    """
+    function to get the focused window title for autologging
+    from: https://stackoverflow.com/questions/10266281/obtain-active-window-using-python
+    """
     hWnd = windll.user32.GetForegroundWindow()
     length = windll.user32.GetWindowTextLengthW(hWnd)
     buf = create_unicode_buffer(length + 1)
@@ -27,123 +33,24 @@ def getForegroundWindowTitle() -> Optional[str]:
     else:
         return None
 
-
-"""
-VARIABLES INIT
-""" 
-running = False
-autoactive = False
-start_time= 0
-activity_start= None
-active=False
-activity_time= 0
-listactivities=[]
-listtimes=[]
-activity_text="None"
-
-"""
-GUI INIT
-"""
-
-# ***** WIDGETS *****
-# create main window
-root = tk.Tk()
-root.geometry('485x320')
-root.title('Moktime')
-activity=tk.StringVar()
-activity.set("Current Activity: "+activity_text)
-autologger=tk.StringVar()
-autologger.set("Enable Autologger")
-
-"""
-NOTIFICATIONS
-"""
-def notify(msg):
-    toaster = ToastNotifier()
-    toaster.show_toast("Moktime", str(msg), icon_path='timer/mug.ico', duration=2, threaded=True)
+def getIdleTime():
+    """
+    function the get current windows user idle time 
+    from: https://stackoverflow.com/questions/911856/detecting-idle-time-using-python
+    """
+    return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0
 
 
-"""
-STOPWATCH
-"""
-#functions
-def start():
-    global running
-    print("start")
-    global start_time
-    global activity_start
-    if start_time==0: #new stopwatch
-        start_time=datetime.datetime.now()
-    else: #take into account paused time
-        global pause_time
-        print(start_time)
-        difference_time = (datetime.datetime.now()-pause_time)
-        print(difference_time)
-        start_time = start_time + difference_time
-        if active:
-            activity_start = activity_start + difference_time
-        print(start_time)
-    if not running:
-        print("calling update")
-        update()
-        print("update called")
-        running = True
-
-def pause():
-    global running
-    if running: #pause and register when to compensate later
-        stopwatch_label.after_cancel(update_time)
-        global pause_time
-        pause_time = datetime.datetime.now()
-        running = False
-
-def switch():
-    global running
-    if running:
-        pause()
-        notify("Timer paused.")
-    else:
-        start()
-        notify("Timer started.")
-
-def reset(): #set everything to zero
-    global running
-    print("reset")
-    if running:
-        stopwatch_label.after_cancel(update_time)
-        running = False
-    global total_time 
-    total_time = 0
-    global start_time
-    start_time = 0
-    #stopwatch_label.config(text='0:00:00') not needed?
-
-def update(): #calculate time by difference between now and the start, round to seconds
-    global total_time
-    global activity_time
-    global activity_start
-    global autoactive
-    print("update")
-    total_time = datetime.datetime.now()-start_time
-    if active:
-        print("update activitytime")
-        activity_time= roundTime(datetime.datetime.now()-activity_start)
-        print("activity time updated")
-        if autoactive:
-            currentfocus = getForegroundWindowTitle()
-            listtimes[-1]=activity_time.total_seconds()
-            print(currentfocus)
-            if listactivities[-1]!=currentfocus:
-                activity_start=datetime.datetime.now()                               
-                listactivities.append(currentfocus)
-                listtimes.append(roundTime(datetime.datetime.now()-activity_start).total_seconds())
-
-
-
-    stopwatch_label.config(text=str(roundTime(total_time)))  
-    print(total_time)
-    global update_time
-    update_time = stopwatch_label.after(1000, update)
+def active_window_process_name():
+    """
+    function to get process name of active window
+    from: https://stackoverflow.com/questions/14394513/win32gui-get-the-current-active-application-name
+    """
+    try:
+        pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow()) #This produces a list of PIDs active window relates to
+        return psutil.Process(pid[-1]).name() #pid[-1] is the most likely to survive last longer
+    except:
+        pass
 
 def roundTime(dt=None, date_delta=datetime.timedelta(seconds=1), to='average'):
     """
@@ -170,60 +77,246 @@ def roundTime(dt=None, date_delta=datetime.timedelta(seconds=1), to='average'):
 
     return dt + datetime.timedelta(0, rounding - seconds, - dt.microseconds)
 
-def autoActivity():
+
+"""
+VARIABLES INIT
+""" 
+running = False
+autoactive = False
+start_time= 0
+activity_start= datetime.datetime.now()
+activity_time= 0
+listactivities=[]
+listtimes=[]
+activity_text=""
+listprocess=[]
+liststart=[]
+listpause=[]
+listplan=[]
+pause_time=0
+notifiedidle=True
+
+
+timeout = 60 #standard timeout time in seconds
+
+"""
+GUI INIT
+"""
+
+# ***** WIDGETS *****
+# create main window
+root = tk.Tk()
+root.geometry('485x320')
+root.title('Moktime')
+activity=tk.StringVar()
+activity.set("Current Activity: "+activity_text)
+autologger=tk.StringVar()
+autologger.set("Enable Autologger")
+startvar=tk.StringVar()
+startvar.set("Start new timer")
+timeoutvar=tk.IntVar()
+timeoutvar.set(timeout)
+
+"""
+NOTIFICATIONS FUNCTION
+"""
+def notify(msg):
+    toaster = ToastNotifier()
+    toaster.show_toast("Moktime", str(msg), icon_path='timer/mug.ico', duration=2, threaded=True)
+
+
+"""
+MAIN PROGRAM 
+"""
+
+def start():
+    """
+    Starts the timer. If previously paused log the paused time.
+    """
+    global running
+    global start_time
+    global activity_start
+    global pause_time
+
+    if start_time==0: #new stopwatch
+        start_time=datetime.datetime.now()
+    else: #take into account paused time
+        global pause_time
+        difference_time = roundTime((datetime.datetime.now()-pause_time))
+        listpause[-1] = difference_time
+        start_time = start_time + difference_time
+        activity_start = activity_start + difference_time
+    if not running:
+        startvar.set("Pause timer")
+        update()
+        running = True
+
+def pause(msg=""):
+    """
+    Stop updating and mark the time when paused.
+    Optional: give string for notification.
+    """
+    global pause_time
+    global running
+    if running: #pause and register when to compensate later
+        startvar.set("Resume timer")
+        stopwatch_label.after_cancel(update_time)
+        global pause_time
+        pause_time = datetime.datetime.now()
+        running = False
+    
+    if not (msg==""):
+        notify(msg)
+
+def switch():
+    """
+    Switch between running and stopped.
+    """
+    global running
+    if running:
+        startvar.set("Resume timer")
+        pause("Timer paused.")
+    else:
+        startvar.set("Pause timer")
+        start()
+        notify("Timer started.")
+
+def reset(): 
+    """
+    Reset the global time and current activity starttime to zero.
+    """
+    global running
+    if running:
+        pause()
+    global total_time 
+    global start_time
+    global activity_start
+
+    if not autoactive:
+        logActivity()
+    else:
+        autoActivity()
+
+    startvar.set("Start new timer")
+    activity_start= datetime.datetime.now()
+    total_time = 0
+    start_time = 0
+    stopwatch_label.config(text='00:00:00')
+
+def update(): 
+    
+    """
+    Do stuff while running: calculate elapsed time total and per activity
+    if autologging: handle getting current process info and log it as well
+    Also auto-pauses when no input is detected for a minute
+    TODO: move all list manipulations to central function
+    """
+    global total_time
+    global activity_time
+    global activity_start
     global autoactive
-    global active
+    global notifiedidle
+    
+    total_time = datetime.datetime.now()-start_time
+    timeout=int(timeout_entry.get())
+    activity_time= roundTime(datetime.datetime.now()-activity_start)
+    if autoactive:
+        currentfocus = getForegroundWindowTitle()
+        currentprocess = active_window_process_name()
+        listtimes[-1]=activity_time.total_seconds()
+        if listactivities[-1]!=currentfocus:
+            activity_start=datetime.datetime.now()
+            liststart.append(activity_start)                               
+            listactivities.append(currentfocus)
+            listprocess.append(currentprocess)
+            listtimes.append(roundTime(datetime.datetime.now()-activity_start).total_seconds())
+            listpause.append(0)
+            listplan.append(activity_text)
+
+    stopwatch_label.config(text=str(roundTime(total_time)))  
+    global update_time
+    update_time = stopwatch_label.after(1000, update)
+
+    idletime=getIdleTime()
+    if idletime>timeout and notifiedidle:
+        notify("You've been idle for "+str(timeout)+" seconds. Logging will autopause in 5 seconds if no further input is detected.")
+        notifiedidle=False
+    if idletime<timeout and not notifiedidle:
+        notifiedidle=True
+    if idletime>timeout+5:
+        notifiedidle=True
+        pause("Autopausing logging after "+str(timeout+5)+" seconds of detected idletime.")
+
+
+
+def autoActivity():
+    """
+    Enable autologging of current proces and time spent.
+    Switchable function, if called it switches between autlogging/stop autologging.
+    TODO: move all list manipulations to central function
+    """
+    global autoactive
     global activity_start
     global listactivities
+    global listprocess
+    global activity_text
 
     if not autoactive:
         autoactive = True
         autologger.set("Disable Autologger")
         pause()
-        active = True
         activity_start = datetime.datetime.now()
         if not listactivities:
             listactivities.append(getForegroundWindowTitle())
+            listprocess.append(active_window_process_name())
+            listplan.append(activity_text)
+            liststart.append(activity_start)
+            listpause.append(0)
         if not listtimes:
             listtimes.append(roundTime(datetime.datetime.now()-activity_start).total_seconds())
         activity.set("Logging active window")
         notify("Autologging started, timer started.")
+        log_button.config(state="disabled")
         start()
     else:
         autoactive = False
         autologger.set("Enable Autologger")
-        active = False
-        activity.set("None")
-        notify("Autologging stopped, timer paused.")
-        pause()
+        log_button.config(state="normal")
+        pause("Autologging stopped, timer paused.")
 
 
    
 def startActivity():
+    """
+    Add an activity title.
+    TODO: move all list manipulations to central function
+    """
     global listactivities
     global activity_time
     global activity_start
-    global active
     global activity
     global autoactive
     global activity_text
 
-    autoactive = False
     pause()
-    active = True
     activity_text=activity_entry.get()
     activity.set("Current Activity: "+activity_text)
     activity_start = datetime.datetime.now()
-    listactivities.append(activity_text)
+    listactivities.append("")
+    liststart.append(activity_start)
+    listprocess.append("Manual Log")
+    listpause.append(0)
+    listplan.append(activity_text)
     notify("Activity "+activity_text+" started!")
-    print(listactivities)
-    print("trying to start")
     start()
 
 def logActivity():
+    """
+    Update time on current activity. 
+    TODO: move all list manipulations to central function
+    """
     global listtimes
     global activity_time
-    global active
     if not activity_time==0:
         pause()
         if len(listactivities) != len(listtimes):
@@ -232,53 +325,96 @@ def logActivity():
             listtimes[-1]=activity_time.total_seconds()
         notify("Logged activity "+activity_text+" for "+str(listtimes[-1]))
         start()
-        print(listtimes)
+
+def updateLists():
+    """
+    Depending on state: update all logging lists.
+    TODO: make this thing and replace
+    """
+
 
 def saveQuit():
+    """
+    Finish up logging the activity and pause time if needed.
+    Zip data into dataframe and save to file.
+    Stop the program.
+    """
     global df
     global listtimes
     global listactivities
-    df = pd.DataFrame(list(zip(listactivities, listtimes)),
-               columns =['Activity / Window', 'Time (s)'])
-    df.to_excel("output.xlsx")  
+
+    if not autoactive:
+        logActivity()
+    if not running and pause_time>0:
+        difference_time = roundTime((datetime.datetime.now()-pause_time))
+        listpause[-1] = difference_time
+    
+    df = pd.DataFrame(list(zip(listplan, listactivities, listprocess, liststart, listtimes, listpause )),
+               columns =['Activity', 'Window', 'Software', 'Start time', 'Time spent (s)', 'Time paused (s)'])
+    try:
+        df.to_csv("output.csv")
+    except PermissionError:
+        timestring = datetime.datetime.now().strftime("%m%d%Y%H%M%S")
+        print("Cannot save, output.csv is currently in use. Using alternative name output"+timestring+".csv")
+        df.to_csv("output"+datetime.datetime.now().strftime("%m%d%Y%H%M%S")+".csv")    
     root.destroy()
 
 """
 BUILD GUI
+TODO: fix layout
 """
 
-# label to display time
-stopwatch_label = tk.Label(root, text='0:00:00', font=('Arial', 30))
-stopwatch_label.pack(fill='x')
 
+
+
+# label to display time
+stopwatch_label = tk.Label(root, text='00:00:00', font=('Arial', 30))
+#stopwatch_label.pack(fill='x')
+stopwatch_label.grid(column=0,row=0,columnspan=2)
 #activity display and logging
 
 
-activity_label = tk.Label(root, textvariable=activity, font=('Arial', 14))
-activity_label.pack(fill='x')
-activity_entry = tk.Entry(root)
-activity_entry.pack(fill='x')
-activity_button = tk.Button(text='Start new activity', height=2, width=7, font=('Arial', 12), command=startActivity)
-activity_button.pack(fill='x')
-log_button = tk.Button(text='Log activitytime', height=2, width=7, font=('Arial', 12), command=logActivity)
-log_button.pack(fill='x')
-log_button = tk.Button(textvariable=autologger, height=2, width=7, font=('Arial', 12), command=autoActivity)
-log_button.pack(fill='x')
-# start, pause, reset, quit buttons
-start_button = tk.Button(text='start', height=1, width=7, font=('Arial', 12), command=start)
-start_button.pack(expand=True, fill='both', side='left')
-pause_button = tk.Button(text='pause', height=1, width=7, font=('Arial', 12), command=pause)
-pause_button.pack(expand=True, fill='both', side='left')
-reset_button = tk.Button(text='reset', height=1, width=7, font=('Arial', 12), command=reset)
-reset_button.pack(expand=True, fill='both', side='left')
-quit_button = tk.Button(text='save & quit', height=1, width=7, font=('Arial', 12), command=saveQuit)
-quit_button.pack(expand=True, fill='both', side='left')
+#activity_label = tk.Label(root, textvariable=activity, font=('Arial', 14))
+#activity_label.pack(fill='x')
+#activity_label.grid(column=0,row=1,columnspan=3)
+
+activity_entry = tk.Entry(root, textvariable=activity)
+#activity_entry.pack(fill='x')
+activity_entry.grid(column=0,row=1,columnspan=2)
+
+activity_button = tk.Button(text='Set title current plan', font=('Arial', 12), command=startActivity)
+#activity_button.pack(fill='x')
+activity_button.grid(column=0,row=2)
+
+log_button = tk.Button(text='Log activity time', font=('Arial', 12), command=logActivity)
+log_button.grid(column=1,row=2)
+#log_button.pack(expand=True, fill='both',side='left')
+
+timeout_entry = tk.Entry(root,textvariable=timeoutvar)
+timeout_entry.grid(column=2,row=2)
+#timeout_entry.pack(expand=True, fill='both',side='left')
+
+autolog_button = tk.Button(textvariable=autologger, font=('Arial', 12), command=autoActivity)
+#autolog_button.pack(fill='x')
+log_button.grid(column=0,row=3, columnspan=2)
+
+start_button = tk.Button(textvariable=startvar, font=('Arial', 12), command=switch)
+start_button.grid(column=0,row=4)
+#start_button.pack(expand=True, fill='both', side='left')
+
+reset_button = tk.Button(text='reset',  font=('Arial', 12), command=reset)
+reset_button.grid(column=1,row=4)
+#reset_button.pack(expand=True, fill='both', side='left')
+
+quit_button = tk.Button(text='save & quit',font=('Arial', 12), command=saveQuit)
+quit_button.grid(column=2,row=4)
+#quit_button.pack(expand=True, fill='both', side='left')
 
 """
 GLOBAL HOTKEYS
 """
 keyboard.add_hotkey('ctrl+space', switch)
-
+keyboard.add_hotkey('shift+space', autoActivity)
 """
 MAIN LOOP
 """
